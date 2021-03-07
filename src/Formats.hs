fc343cba784aeaa5759b4f2eb2291986dfff429e
leiftw <leif@leif.tw>
2021-03-07 04:31:11 +0100

    capitalize file names of modules to be exposed re-export only actual `Format`s in `Formats` instead of catch-all `Lib`

--- src/Formats.hs
+++ src/Formats.hs
@@ -1,19 +1,11 @@
-module Lib
+module Formats
     ( module Format
     , module Patterns
     , module Operators
     , module Utils
-    , module Foread
-    , module Forite
-    , module Formed
     ) where
 
 import Format
 import Patterns
 import Operators
 import Utils
-
-import Foread
-import Forite
-
-import Formed
