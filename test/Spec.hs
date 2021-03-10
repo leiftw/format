4f378e0046e1eeea9979e592ea9de67160f954a4
leiftw <leif@leif.tw>
2021-03-10 15:59:33 +0100

    incorporate `Data.Data` utility into test `Format`

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 --module Spec where
 
+import Data.Data
 import Data.Maybe (fromJust)
 
 import Formats
@@ -10,9 +13,18 @@ import Forite
 import Foread
 
 
-fo :: Format String
-fo = FAtom Just id --F2In const id (FAtom Just id) "_" null (FC_ "the empty string")
+data Tripli = Primo | Alto Bool | Ultimo Int deriving Data
+
+tripliform :: Format Tripli
+tripliform = FCond ((=="Primo").showConstr.toConstr) -- avoid hard string?
+                   (FConst Primo "ONE")
+             (FConst (Alto False) "MORE")
+
+--fo :: Format String
+--fo = FAtom Just id
+--fo = F2In const id (FAtom Just id) "_" null (FC_ "the empty string")
 
 
 main :: IO ()
-main = putStrLn $ forite fo $ fromJust $ foread fo "yo"
+main = putStrLn $ forite tripliform
+     $ fromJust $ foread tripliform "MORE"
